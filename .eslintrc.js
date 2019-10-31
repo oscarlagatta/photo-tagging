8f67ac22d5f771068ea60edf9d3eecf596f3995a
Oscar Lagatta <Oscar.Lagatta@jato.com>
2019-10-31 16:42:51 +0000

    esling configuration

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+    "parser": "babel-eslint",
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es6": true,
+        "node": true,
+        "jest": true
+    },
+    "settings": {
+        "react": {
+            "version": "16.11.0"
+        }
+    },
+    "extends": ["eslint:recommended", "plugin:react/recommended"],
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": { "react/no-unescaped-entities": 0 }
+};
